8039e2504ff0e5dd766f18caab4134b8517ea3d8
eunicek4632 <20121G040@gmail.com>
2021-01-24 21:06:33 +0800

    Debug and added cloud base line

--- script.js
+++ script.js
@@ -29,7 +29,13 @@ var height = new Array();
 var temp = new Array();
 var pressure = new Array(1006, 1000, 950, 900, 850, 800, 700, 600, 500);
 var dew_pt = new Array();
+var cloud_base_height = 0;
 
+// Set the range of values in x-axis
+var max_x = 270;
+var min_x = 220;
+
+// Send POST request to obtain weather data from Windy
 var xhr = new XMLHttpRequest();
 xhr.open("POST", url, true);
 xhr.setRequestHeader("Content-Type", "application/json");
@@ -40,30 +46,34 @@ xhr.onreadystatechange = function () {
         pressure[0] = (json['pressure-surface'][0] / 100);
         data_org();
         calc_height();
+        find_cloud_base();
+        set_margin();
+        console.log("Average Temperature");
+        console.log(avg_temp_arr);
+        console.log("Dew Point");
+        console.log(dew_pt);
+        console.log("Temp");
+        console.log(temp);
         graph();
     }
 };
 var data = JSON.stringify({"lat": 42, "lon": 12, "model": "gfs", "parameters": ["temp", "dewpoint", "pressure"], "levels": ["surface", "1000h", "950h", "900h", "850h", "800h", "700h", "600h", "500h"], "key": "1ZAR25ecRJ7pghoENmV77c9v48cvrrkq"});
 xhr.send(data);
 
+// Set the range of x axis according to the data's max and min
+function set_margin(){
+    for(var i = 0; i < 9; i++){
+        if(temp[i] < min_x){
+            min_x = temp[i];
+        }
+        if(temp[i] > max_x){
+            max_x = temp[i];
+        }
+    }
+}
 
-/*
-windyInit(options, windyAPI => {
-    // windyAPI is ready, and contain 'map', 'store',
-    // 'picker' and other usefull stuff
-    const { map, store, broadcast, picker, utils } = windyAPI;
-
-    const levels = store.getAllowed('availLevels');
-    console.log("Levels");
-    console.log(levels);
-    
-    L.popup()
-        .setLatLng([22.347536, 114.142456])
-        .setContent('Hong Kong is here')
-        .openOn(map);
-});
-*/
-//only keep necessary data for processing
+// only keep necessary data for processing
+// onlt the first entry in each array is left
 function drop_data(){
     for(var k in json){
         if(k == "warning"){
@@ -81,6 +91,7 @@ function data_org(){
     //i keeps track of the elements to take from json and the number of elements to take average from
     var i = 0;
     var avg = 0;
+    console.log(json)
     for(var k in json) {
         if(k == "ts" || k == "units"){
             continue;
@@ -92,23 +103,18 @@ function data_org(){
             avg_temp_arr.push(avg);
 
         }
-        if(i >= 9){
+        if(i >= 9 && i <= 17){
             //add dew point temperature of each pressure level to the array 
             dew_pt.push(json[k][0]);
         }
         i++;
     }
-    // console.log("Temperature");
-    // console.log(avg_temp_arr);
-    // console.log("Dew Point");
-    // console.log(dew_pt);
 }
 
 //calculate height from pressure and temperature
 function calc_height(){
     //  Z = RT/g ln(ps / p)     R = 287     g = 9.807
-    height.push(0);
-    for(var i = 1; i < 9; i++){
+    for(var i = 0; i < 9; i++){
         var z = (R * avg_temp_arr[i] / g) * Math.log(pressure[0] / pressure[i]);
         height.push(z);
     }
@@ -116,12 +122,22 @@ function calc_height(){
     // console.log(height);
 }
 
+function find_cloud_base(){
+    for(var i = 0; i < 9; i++){
+        if(avg_temp_arr[i] <= dew_pt[i] && height[i] > 0){
+            cloud_base_height = height[i];
+            break;
+        }
+    }
+}
+
 //Plotly
+// Plot graph
 function graph(){
 
     var lineDiv = document.getElementById('tester');
 
-    var traceA = {
+    var temp_line = {
         x: temp, 
         y: height,  
         type: 'scatter',
@@ -132,7 +148,7 @@ function graph(){
         name: 'Temperature'
     };
 
-    var traceB = {
+    var dew_line = {
         x: dew_pt, 
         y: height,  
         type: 'scatter',
@@ -143,7 +159,24 @@ function graph(){
         name: 'Dew Point'
     }
     
-    var data = [traceA, traceB];
+    var cloud_base = {
+        x: [min_x + 10, max_x + 10], 
+        y: [cloud_base_height, cloud_base_height],  
+        type: 'scatter',
+        line: {
+            color: 'rgb(0, 100, 0)',
+            width: 1
+        },
+        name: 'Cloud base'
+    }
+    var data;
+    if(cloud_base_height <= 0){
+        data = [temp_line, dew_line];
+    }
+    else{
+        data = [temp_line, dew_line, cloud_base];
+    }
+    
     
     var layout = {
     title:'Atmosphere Conditions',
@@ -163,7 +196,7 @@ function graph(){
         color: 'rgb(100,150,200)',
         size: 16
         },
-        range: [200, 350]
+        range: [min_x - 10, max_x + 10]
     },
     yaxis: {
         title: 'Height (m)',
